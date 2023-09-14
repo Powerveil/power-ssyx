22caa9038c970759055fb1f2924105a06205bf66
Powerveil <1968103220@qq.com>
2023-09-14 18:27:39 +0800

    用户模块的创建和一些配置

---
+++ service/service-user/src/main/java/com/power/ssyx/ServiceUserApplication.java
@@ -0,0 +1,20 @@
+package com.power.ssyx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author Powerveil
+ * @Date 2023/9/14 17:40
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class ServiceUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceUserApplication.class, args);
+    }
+}
