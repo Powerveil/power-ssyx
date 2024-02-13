4d92fc11c2923486caf34b1697004b8a2f70b403
Powerveil <1968103220@qq.com>
2024-02-13 10:54:21 +0800

    定时任务删除redis文件上传黑名单中的key

--- service/service-product/src/main/java/com/power/ssyx/ServiceProductApplication.java
+++ service/service-product/src/main/java/com/power/ssyx/ServiceProductApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author Powerveil
@@ -12,7 +13,7 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @SpringBootApplication
 @EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true) // 注意这里的 exposeProxy 设置为 true
 @EnableDiscoveryClient
-//@EnableScheduling
+@EnableScheduling
 public class ServiceProductApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceProductApplication.class, args);
--- service/service-product/src/main/java/com/power/ssyx/product/job/UpdateBlackListJob.java
+++ service/service-product/src/main/java/com/power/ssyx/product/job/UpdateBlackListJob.java
@@ -1,55 +1,35 @@
-//package com.power.ssyx.product.job;
-//
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.data.redis.cache.RedisCache;
-//import org.springframework.data.redis.core.Cursor;
-//import org.springframework.data.redis.core.RedisTemplate;
-//import org.springframework.data.redis.core.ScanOptions;
-//import org.springframework.data.redis.core.ValueOperations;
-//import org.springframework.scheduling.annotation.Scheduled;
-//import org.springframework.stereotype.Component;
-//
-//import java.io.UnsupportedEncodingException;
-//import java.util.Map;
-//import java.util.Objects;
-//import java.util.Set;
-//
-///**
-// * @ClassName UpdateBlackListJob
-// * @Description 更新redis文件上传黑名单
-// * @Author Powerveil
-// * @Date 2024/2/4 21:39
-// * @Version 1.0
-// */
-//@Component
-//@Slf4j
-//public class UpdateBlackListJob {
-//
-//    @Autowired
-//    private RedisTemplate redisTemplate;
-//
-//
-//    // 注解表示每隔12小时执行一次
-//    @Scheduled(fixedRate = 12 * 60 * 60 * 1000)
-//    public void deleteExpiredUploadBlacklistKeys() throws UnsupportedEncodingException {
-//// 使用 SCAN 命令进行迭代
-//        ScanOptions options = ScanOptions.scanOptions().match("upload:blacklist:*").build();
-//        Cursor<byte[]> cursor = redisTemplate.getConnectionFactory().getConnection().scan(options);
-//
-//        while (cursor.hasNext()) {
-//            byte[] rawKey = cursor.next();
-//            String key = new String(rawKey, String.valueOf(redisTemplate.getStringSerializer()));
-//
-//            // 检查键是否过期
-//            Long ttl = redisTemplate.getExpire(key);
-//
-//            // 如果过期时间小于等于0，表示键已过期
-//            if (ttl != null && ttl <= 0) {
-//                // 删除过期键
-//                redisTemplate.delete(key);
-//                System.out.println("Deleted expired key: " + key);
-//            }
-//        }
-//    }
-//}
+package com.power.ssyx.product.job;
+
+import com.power.ssyx.common.constant.RedisConst;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+
+/**
+ * @ClassName UpdateBlackListJob
+ * @Description 定时任务删除redis文件上传黑名单中的key
+ * @Author Powerveil
+ * @Date 2024/2/4 21:39
+ * @Version 1.0
+ */
+@Component
+@Slf4j
+public class UpdateBlackListJob {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+
+    // 注解表示 每天中午12点触发
+    @Scheduled(cron = "0 0 12 * * ?")
+    public void deleteExpiredUploadBlacklistKeys() {
+        // 暂时没有特定规则,就是定时删除所有的key
+        Set<String> keys =
+                redisTemplate.keys(RedisConst.FILE_UPLOAD_BLACK_LIST_KEY_PREFIX + "*");
+        redisTemplate.delete(keys);
+    }
+}
